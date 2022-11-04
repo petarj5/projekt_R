b87f36e0a0f3d8be71a6b4614585f738f95b13fe
petar321 <pj53517@fer.hr>
2022-11-04 01:53:20 +0100

    io.c fixed, added test to write in file

--- zadaca_2/exesrc/test_file.c
+++ zadaca_2/exesrc/test_file.c
@@ -1,15 +1,9 @@
 #include <stdio.h>
-#include "io.h"
-#include <fcntl.h>  
+#include <fcntl.h>
+#include "generator.h"  
 
 int main(void) {
-	const char *inputFilePath = "../resources/input.txt"; 
-	const char *outputFilePath = "../resources/output.txt";
 
-	int fd = open(inputFilePath, O_RDONLY | O_EXCL);
-
-	printf("%d\n", fd);
-
-
+	generateSignal(200);
 
 }
--- zadaca_2/src/io.c
+++ zadaca_2/src/io.c
@@ -1,42 +1,26 @@
 #include "io.h"
 #include <unistd.h>
 #include <string.h>
-
-#define WORD_SIZE_IN_BITS 32
-#define HALF_WORD_SIZE_IN_BITS 16
-
+#include <stdio.h>
 
 int read_word(int fd) {
 	int retval;
-	char buf[WORD_SIZE_IN_BITS];
 
-	read(fd, buf, WORD_SIZE_IN_BITS);
-	memcpy(&retval, buf, sizeof(short));
-	
+	read(fd, &retval, sizeof(int));
 	return retval;
 }
 
 short read_half(int fd) {
 	short retval;
-	char buf[HALF_WORD_SIZE_IN_BITS];
-
-	read(fd, buf, HALF_WORD_SIZE_IN_BITS);
-	memcpy(&retval, buf, sizeof(short));
-
+	read(fd, &retval, sizeof(short));
 	return retval; 
 }
 
 void write_word(int fd, int word) {
-	char buf[WORD_SIZE_IN_BITS];
-
-	memcpy(&word, buf, sizeof(int));
-	write(fd, buf, WORD_SIZE_IN_BITS);
+	write(fd, &word, sizeof(int));
 }
 
 
 void write_half(int fd, short word) {
-	char buf[HALF_WORD_SIZE_IN_BITS];
-
-	memcpy(&word, buf, sizeof(short));
-	write(fd, buf, HALF_WORD_SIZE_IN_BITS);
+	write(fd, &word, sizeof(short));
 }
